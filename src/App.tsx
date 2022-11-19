431e62016647109195789144e5bdbde9eee308c0
preska <wolfwar@outlook.com>
2022-11-19 11:32:29 +0100

    create note handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,30 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useMemo, useState } from "react";
+import { v4 as uuidV4 } from "uuid";
+
 import { Container } from "react-bootstrap";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { NewNote } from "./NewNote";
 import { useLocalStorage } from "./useLocalStorage";
 
-export type RowNote = {
-  id: string;
-} & RowNoteData;
-
 export type Note = {
   id: string;
-} & RowNoteData;
+} & NoteData;
 
-export type RowNoteData = {
+export type RawNote = {
+  id: string;
+} & RawNoteData;
+
+export type RawNoteData = {
   title: string;
   markdown: string;
   tagIds: string[];
+};
+
+export type NoteData = {
+  title: string;
+  markdown: string;
+  tags: Tag[];
 };
 
 export type Tag = {
@@ -25,7 +33,7 @@ export type Tag = {
 };
 
 function App() {
-  const [notes, setNotes] = useLocalStorage<RowNote[]>("NOTES", []);
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", []);
   const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", []);
 
   const notesWithTags = useMemo(() => {
@@ -35,11 +43,20 @@ function App() {
     }));
   }, [notes, tags]);
 
+  const onCreateNote = ({ tags, ...data }: NoteData) => {
+    setNotes((prevNotes) => {
+      return [
+        ...prevNotes,
+        { ...data, id: uuidV4(), tagIds: tags.map((tag) => tag.id) },
+      ];
+    });
+  };
+
   return (
     <Container className="my-4">
       <Routes>
         <Route path="/" element={<h1>Hi</h1>} />
-        <Route path="/new" element={<NewNote />} />
+        <Route path="/new" element={<NewNote onSubmit={onCreateNote} />} />
         <Route path="/:id">
           <Route index element={<h1>Show</h1>} />
           <Route path="edit" element={<h1>Edit</h1>} />
